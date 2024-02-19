6d4fd6e88c072624d5974cb26b15eccb9c55a6a2
juri931 <impa.mat@hotmail.it>
2024-02-19 14:11:04 +0100

    [add] Milestone 1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//1
+const firstName = prompt('Qual è il tuo nome?');
+// console.log(`Il nome è ${firstName}`);
+
+//2
+const lastName = prompt('Qual è il tuo cognome?');
+// console.log(`Il cognome è ${lastName}`);
+
+//3
+const color = prompt('Qual è il tuo colore preferito?');
+// console.log(`Il colore preferito è ${color}`);
+
+//4
+const pass = firstName + lastName + color + 24;
+console.log(pass);
